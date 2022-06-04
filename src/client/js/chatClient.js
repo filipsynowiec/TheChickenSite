7ed2bbdbca578f4fd7ffb6ab985adc424a52ff24
Zofia Glapa <zofia.glapa@student.uj.edu.pl>
2022-06-04 19:10:08 +0200

    modified chat history to return a list of messages

--- src/client/js/chatClient.js
+++ src/client/js/chatClient.js
@@ -3,10 +3,13 @@ class ChatClient {
     this._socket = socket;
     let instance = this;
     this._socket.on("updateChat", (data) => {
-      instance.updateHTML("chat_history", data.chatHistory);
+      instance.updateHTML("chat-history", data.chatHistory);
       console.log("Received chat history");
     });
-    document.getElementById("send_message").onclick = () => {
+    document.getElementById("message").onkeydown = (event) => {
+      if (event.key === 'Enter') instance.sendChatMessage();
+    }
+    document.getElementById("send-message").onclick = () => {
       instance.sendChatMessage();
     };
     getName(
@@ -19,11 +22,20 @@ class ChatClient {
     );
   }
   updateHTML(element, data) {
-    document.getElementById(element).innerHTML = data;
+    let template = document.getElementById("chat-row-template");
+    document.getElementById(element).innerHTML = '';
+    for (let i = 0; i < data.length; i++) {
+      let htmlElement = template.content.firstElementChild.cloneNode(true);
+      htmlElement.innerHTML = `${data[i].user}: ${data[i].message}`;
+      document.getElementById(element).appendChild(htmlElement);
+    }
   }
   sendChatMessage() {
     let mes = document.getElementById("message").value;
-    let name = instance._name;
+    document.getElementById("message").value = null;
+    let element = document.getElementById("chat-history");
+    element.scrollTop = element.scrollHeight;
+    let name = this._name;
     this._socket.emit("sendChatMessage", {
       name: name,
       value: mes,

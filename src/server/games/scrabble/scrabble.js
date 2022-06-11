1ee5b81228179612ff2387e9dccfb8f9850396e8
Roch Wojtowicz <roch.wojtowicz@student.uj.edu.pl>
2022-06-11 22:16:22 +0200

    Implemented simple scrabble

--- src/server/games/scrabble/scrabble.js
+++ src/server/games/scrabble/scrabble.js
@@ -1,9 +1,9 @@
 const { logger } = require("../../../utils/logger");
 const { Deck } = require("./deck");
 
-const BOARD_SIZE = 15
-const NR_OF_PLAYERS = 2
-const TILES_ON_HAND = 7
+const BOARD_SIZE = 15;
+const NR_OF_PLAYERS = 2;
+const TILES_ON_HAND = 7;
 
 class Scrabble {
     constructor(seats) {
@@ -26,7 +26,7 @@ class Scrabble {
                 this._board[i][j] = null;
             }
         }
-        this._deck = new Deck().deck;
+        this._deck = Deck.getDeck();
         for (let i = 0; i < NR_OF_PLAYERS; i++) {
             this._hands[i] = [];
             for(let j=0; j<TILES_ON_HAND && this._deck.length>0; ++j) {
@@ -35,26 +35,79 @@ class Scrabble {
             this._scores[i] = 0;
         }
     }
+    checkIfCorrect(changes) { 
+        return true; //temporary
+    }
+    getScore(changes) {
+        let result=0;
+        for(const change of changes) {
+            
+            result += change[0].value; //temporary
+        }
+        console.log(`result ${result}`);
+        return result;
+    }
+    applyChanges(changes) {
+        //console.log(`applyChanges ${changes}`);
+        const handCopy = [...this._hands[this._turn]];
+        if(!this.checkIfCorrect(changes)) {
+            logger.error("Not correct changes!");
+            return;
+        }
+        for(const change of changes) {
+            let index = handCopy.findIndex((tile) => {return tile.letter == change[0].letter});
+            if(index == -1) {
+                logger.error("Used tile not in a hand!");
+                return;
+            }
+            handCopy[index] = handCopy[handCopy.length-1];
+            handCopy.pop();
+        }
+        for(const change of changes) {
+            if(this._board[change[1]][change[2]] != null) {
+                logger.error("Filling already filled tile!");
+                return;
+            }
+            this._board[change[1]][change[2]] = change[0];
+        }
+        while(handCopy.length<TILES_ON_HAND && this._deck.length>0) {
+            handCopy.push(this._deck.pop());
+        }
+        if(this._deck.length==0) {
+            this._end = true;
+            this._seats.gameEnded();
+        }
+        this._hands[this._turn] = [...handCopy];
+        this._scores[this._turn] += this.getScore(changes);
+        this._turn = (this._turn+1)%NR_OF_PLAYERS;
+    }
     restart() {
         this.setStartingState();
+        this._turn = 0;
         this._observers.forEach((observer) => observer.sendStatus());
     }
     updateGame(data) {
-        this.applyChanges(data.changes); //to be written
+        this.applyChanges(data.changes);
         this._observers.forEach((observer) => observer.sendStatus());
     }
     getStatus() {
+        let hands = {};
+        let scores = {};
+        for(let i=0; i<NR_OF_PLAYERS; ++i) {
+            hands[this._seats.seats[i]] = [...this._hands[i]];
+            scores[this._seats.seats[i]] = this._scores[i];
+        }
+        
         return {
-            turn: this._turn,
-            end: this._end,
+            running: this._seats.getRunning(),
             board: this._board,
-            scores: this._scores,
-            hands: this._hands,
+            scores: scores,
+            hands: hands,
             active: this._seats.seats[this._turn],
         }
     }
     getHTMLLocation() {
-        return "server/games/scrabble/scrabble.html";
+        return "src/server/games/scrabble/scrabble.html";
     }
 }
 

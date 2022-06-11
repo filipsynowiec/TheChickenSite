833b5ff8aee43a77ca6e872a1d1c3d7a95cd9042
Zofia Glapa <zofia.glapa@student.uj.edu.pl>
2022-06-12 00:12:17 +0200

    fixed broken room routes

--- src/server/serverManagers/clientManager.js
+++ src/server/serverManagers/clientManager.js
@@ -26,7 +26,7 @@ class ClientManager {
     this._basepath = path;
   }
 
-  registerRoomRoute(app, game) {
+  registerRoomRoute(app) {
     let instance = this;
     app.get("/room/:roomId", function (req, res) {
       if (instance._ROOMS[req.params.roomId] == undefined) {
@@ -34,6 +34,7 @@ class ClientManager {
           path.join(instance._basepath, "src/client/html", "index.html")
         );
       } else {
+        let game = instance._roomChoiceManager.getGameFromRoomId(req.params.roomId);
         fs.readFile("src/client/html/room.html", "utf8", (err, data) => {
           if (err) {
             logger.error(err);
@@ -151,7 +152,7 @@ class ClientManager {
       });
       childProcess.on("message", (msg) => {
         let receivedMessage = RoomManager.receiveMessageFromRoom(msg, roomId);
-        
+
         logger.info(
           `Received message from room with keys ${Object.keys(
             receivedMessage
@@ -193,7 +194,7 @@ class ClientManager {
       this._ROOMS[roomId] = childProcess;
       this._roomChoiceManager.addRoom(roomId, game);
 
-      this.registerRoomRoute(app, game);
+      this.registerRoomRoute(app);
       RoomManager.createRoom(app, childProcess, roomId, game);
 
       this.sendToOneClient("roomId", { roomId: roomId }, socketId);
